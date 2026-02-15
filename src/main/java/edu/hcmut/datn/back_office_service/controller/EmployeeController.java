9cd95486907f62b8b39be83406d12c04c7af9500
KhoiTranAnh <trananhkhoitv@gmail.com>
2026-02-15 21:32:45 +0700

    feat: implement Employee management with CRUD operations, including controller, service, repository, and exception handling

---
+++ src/main/java/edu/hcmut/datn/back_office_service/controller/EmployeeController.java
@@ -0,0 +1,72 @@
+package edu.hcmut.datn.back_office_service.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import edu.hcmut.datn.back_office_service.dao.Employee;
+import edu.hcmut.datn.back_office_service.dto.response.ApiResponse;
+import edu.hcmut.datn.back_office_service.service.EmployeeService;
+
+@Controller
+@RequestMapping("/api/employee")
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<Employee>> create(@RequestBody Employee employee) {
+        try {
+            Employee newEmp = employeeService.create(employee);
+
+            return ResponseEntity.ok().body(ApiResponse.SUCCESS(HttpStatus.OK.toString(), "Create employee successfully", newEmp));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(ApiResponse.ERROR(HttpStatus.BAD_REQUEST.toString(), e.getMessage(), null));
+        }
+    }
+
+    @GetMapping("/{employeeId}")
+    public ResponseEntity<ApiResponse<Employee>> read(@PathVariable Long employeeId) {
+        try {
+            Employee emp = employeeService.read(employeeId);
+
+            return ResponseEntity.ok().body(ApiResponse.SUCCESS(HttpStatus.OK.toString(), "Read employee successfully", emp));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(ApiResponse.ERROR(HttpStatus.BAD_REQUEST.toString(), e.getMessage(), null));
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<List<Employee>>> readAll(Integer pageNum, Integer pageSize) {
+        List<Employee> list = employeeService.readAll(pageNum, pageSize);
+
+        if (list.isEmpty()) {
+            return ResponseEntity.ok().body(ApiResponse.SKIP_AS_GOOD(HttpStatus.OK.toString(), "No Employee Exists", null));
+        }
+
+        return ResponseEntity.ok().body(ApiResponse.SUCCESS(HttpStatus.OK.toString(), "Get all employees successfully", list));
+    }
+
+    @DeleteMapping("/{employeeId}")
+    public ResponseEntity<ApiResponse<Void>> delete(@PathVariable Long employeeId) {
+        try {
+            employeeService.delete(employeeId);
+
+            return ResponseEntity.ok().body(ApiResponse.SUCCESS(HttpStatus.OK.toString(), "Delete employee successfully", null));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(ApiResponse.ERROR(HttpStatus.BAD_REQUEST.toString(), e.getMessage(), null));
+        }
+    }
+}
--- src/main/java/edu/hcmut/datn/back_office_service/dao/Employee.java
+++ src/main/java/edu/hcmut/datn/back_office_service/dao/Employee.java
@@ -5,24 +5,39 @@ import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
 
 @Entity
 public class Employee {
-    
+
     @Column(name="emp_id")
     @Id
+    @Getter
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long empId;
-    
+
     @Column(name="hire_date")
+    @Getter
     private LocalDate hireDate;
-    
+
     @Column(name="user_id")
+    @Getter
     private Long userId;
-    
+
     @Column(name="updated_at")
     private LocalDateTime updatedAt;
-    
+
     @Column(name="created_at")
     private LocalDateTime createdAt;
+
+    protected Employee() {}
+
+    // Contructor for creating purpose
+    public Employee(LocalDate hireDate, Long userId) {
+        this.hireDate = hireDate;
+        this.userId = userId;
+    }
 }
